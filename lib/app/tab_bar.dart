b22bc29cefbb5facc66ffa77d8680c33177ab3fa
yosuaelw <cuatiawww@gmail.com>
2022-11-14 06:39:07 +0700

    first commit

--- lib/app/tab_bar.dart
+++ lib/app/tab_bar.dart
@@ -1,3 +1,4 @@
+import 'package:fic_task_shop/common/my_color.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -11,6 +12,19 @@ class TabBottomBar extends StatefulWidget {
 class _TabBottomBarState extends State<TabBottomBar> {
   @override
   Widget build(BuildContext context) {
-    return Row();
+    return Container(
+      height: 64,
+      decoration: BoxDecoration(
+        color: MyColor.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.3),
+            spreadRadius: 2,
+            blurRadius: 15,
+            offset: Offset(0, 5),
+          )
+        ],
+      ),
+    );
   }
 }
